303b0ec916a8381258b8d8f4e90692ff93189a85
suren <suren@localhost.localdomain>
2018-11-07 12:04:01 +0545

    Edited

--- q2.py
+++ q2.py
@@ -1,11 +1,14 @@
 a = 1
 b = 2
+i = 1
 sum = b
-for i in range(1,4000000):
+while b<4000000:
     c = a + b
+
     a = b
     b = c
-    if i%2 == 0:
+    if c%2 == 0:
         sum = sum +c
+    i=i+1
 print(sum)
-
+# 4613732
