4b279ac7d6ead5c896c4d26bd92d0739675b1a6d
suren <suren@localhost.localdomain>
2018-11-13 21:12:00 +0545

    question 5 ko

--- q5.py
+++ q5.py
@@ -1,9 +1,8 @@
 
-for i in range (2,10000000000):
-    count=0
-    for j in range(1,20):
-        if (i%j==0):
-            count=count+1
-            if(count==20):
+for i in range(20, 10000000):
+    count = 0
+    for j in range(1, 21):
+        if (i % j == 0):
+            count = count+1
+            if(count == 20):
                 print(i)
-    
