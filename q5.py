ee7e68e0520c24c5f04aabed94751dc5b2fbffba
Yogendra Tamang <48yogen@gmail.com>
2018-11-13 20:58:12 +0545

    add q5

--- q5.py
+++ q5.py
@@ -1,9 +1,34 @@
+#! user/bin/env python
+"""
+q5.py - Euler Question No. 5
 
-for i in range (2,10000000000):
-    count=0
-    for j in range(1,20):
-        if (i%j==0):
-            count=count+1
-            if(count==20):
-                print(i)
-    
+Question:
+2520 is the smallest number that can be divided by each
+of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly
+divisible by all of the numbers from 1 to 20?
+
+"""
+
+
+def solveQ5(upper):
+    """
+    solves q5
+    """
+    i = 40
+    while True:
+        count = 0
+        print(i)
+        for j in range(1, (upper+1)):
+            if i % j == 0:
+                count += 1
+        if count==upper:
+            return i
+        i += 1
+
+
+
+if __name__ == '__main__':
+    print("Started Main Function")
+    a = solveQ5(20)
+    print("Answer: %s" % a)
