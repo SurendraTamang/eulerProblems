a4ff348f98ef08ac21abb88bc12edaa6251be428
suren <suren@localhost.localdomain>
2018-11-16 15:26:37 +0545

    PEP

--- q1.py
+++ q1.py
@@ -1,15 +1,18 @@
-#! user/bin/env python
+#!usr/bin/env python
 """
-q1.py - Euler Question No. 1
+q1.py - Euler Question No. 1-Multiples of 3 and 5
 
 Question:
 If we list all the natural numbers below 10 that are multiples 
 of 3 or 5,
  we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.
 
+Question Link https://projecteuler.net/problem=1
+
 """
+
+
 def solveQ1(upper_limit):
     """
     It solves question 1
@@ -18,8 +21,10 @@ def solveQ1(upper_limit):
     for i in range(0, upper_limit):
         if i % 3 == 0 or i % 5 == 0:
             sum = sum+i
-    if sum !=0:
-        return sum
+
+    return sum
+
+
 if __name__ == '__main__':
     print("Started Main Function")
     a = solveQ1(1000)
--- q2.py
+++ q2.py
@@ -1,14 +1,39 @@
-a = 1
-b = 2
-i = 1
-sum = b
-while b<4000000:
-    c = a + b
+#!/usr/bin/env python
+"""
+q2.py - Euler Question No. 2- Even Fibonacci numbers
 
-    a = b
-    b = c
-    if c%2 == 0:
-        sum = sum +c
-    i=i+1
-print(sum)
+Question:
+Each new term in the Fibonacci sequence is generated 
+by adding the previous two terms. By starting with 
+1 and 2, the first 10 terms will be:
+
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+By considering the terms in the Fibonacci sequence 
+whose values do not exceed four million, 
+find the sum of the even-valued terms.
+
+Question Link https://projecteuler.net/problem=2
+
+"""
+
+
+def solveQ2(limit):
+    a = 1
+    b = 2
+    sum = b
+    while b < limit:
+        c = a + b
+        a = b
+        b = c
+        if c % 2 == 0:
+            sum = sum + c
+
+    return sum
+
+
 # 4613732
+if __name__ == '__main__':
+    print("Started Main Function")
+    a = solveQ2(4000000)
+    print("Answer: %s" % a)
